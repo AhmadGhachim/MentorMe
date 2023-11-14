4b060ba82d3192940b99d8dd70708d905014557f
Aitazaz Gilani <aitazazgilani@gmail.com>
2023-11-14 14:54:33 -0600

    fixed imports and routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import SignupMentee1 from './pages/SignupMentee1'
 import SignupMentee2 from './pages/SignupMentee2'
 import SignupMentee3 from './pages/SignupMentee3'
 import SignupMentee4 from './pages/SignupMentee4'
-import SignupMentor from './pages/SignUpMentor'
+import SignupMentor1 from './pages/SignupMentor1'
+import SignupMentor2 from './pages/SignupMentor2'
+import SignupMentor3 from './pages/SignupMentor3'
+import SignupMentor4 from './pages/SignupMentor4'
 import Home from './pages/Home'
 import Landing from './pages/Landing'
 import Signup from './pages/Signup';
@@ -24,7 +27,11 @@ function App() {
         <Route path="/SignupMentee3" element={<SignupMentee3 />}/>
         <Route path="/SignupMentee2" element={<SignupMentee2 />}/>
         <Route path="/SignupMentee1" element={<SignupMentee1 />}/>
-        <Route path="/SignupMentor" element={<SignupMentor />}/>
+        <Route path="/SignupMentor1" element={<SignupMentor1 />}/>
+        <Route path="/SignupMentor2" element={<SignupMentor2 />}/>
+        <Route path="/SignupMentor3" element={<SignupMentor3 />}/>
+        <Route path="/SignupMentor4" element={<SignupMentor4 />}/>
+
         <Route path="/FindMentor" element={<FindMentor />}/>
       </Routes>
       </AuthProvider>
